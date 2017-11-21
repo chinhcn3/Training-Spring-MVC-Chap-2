f3682bb49e234c4fe202581adc10984e1310dab5
Vu Duc Chinh <chinhcn3@gmail.com>
2017-11-21 10:41:13 +0700

    update html

---
+++ src/main/java/demo/web/config/DispatcherServletInitializer.java
@@ -0,0 +1,25 @@
+//package demo.web.config;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+///**
+// * Created by ChinhVD on 11/21/17.
+// */
+//public class DispatcherServletInitializer extends
+//        AbstractAnnotationConfigDispatcherServletInitializer {
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return null;
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[]{
+//                WebApplicationContextConfig.class};
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[]{"/"};
+//    }
+//}
--- src/main/java/demo/web/controller/LoginController.java
+++ src/main/java/demo/web/controller/LoginController.java
@@ -1,6 +1,7 @@
 package demo.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @RequestMapping("/home")
-    public String showLogin(){
+    public String welcome() {
         return "login";
     }
 }
