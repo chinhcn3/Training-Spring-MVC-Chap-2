1a4fc157759d1f01952a5841443a18ebc16282dd
Vu Duc Chinh <chinhcn3@gmail.com>
2017-11-21 09:06:59 +0700

    init code

---
+++ src/main/java/demo/web/controller/LoginController.java
@@ -0,0 +1,18 @@
+package demo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ChinhVD on 11/1/17.
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping("/home")
+    public String showLogin(){
+        return "login";
+    }
+}
